4dd2b7f381410c36a40296ce6d4b75045b62bb07
Paul D'Amico <pauldamico88@gmail.com>
2022-12-11 14:48:30 -0500

    added all tracker routes and initial logic

--- routes/trackerRouter.js
+++ routes/trackerRouter.js
@@ -25,4 +25,32 @@ Tracker.find({user:req.auth._id}, (err, foundItems)=>{
     })
 })
 
+trackerRouter.put('/update/:trackerId', (req, res, next)=>{
+Tracker.findOneAndUpdate({user:req.auth._id, _id:req.params.trackerId}, req.body, {new:true}, (err, updatedItem)=>{
+if(err)
+{res.status(500)
+return (next(err))
+}
+res.send(updatedItem)
+
+})
+})
+
+trackerRouter.delete('/delete/:trackerId', (req, res, next)=>{
+const trackerId = req.params.trackerId
+    Tracker.findOneAndDelete({user:req.auth._id, _id:trackerId}, (err, deletedItem)=>{
+if(err){
+    res.status(500)
+    return next(err)
+}
+if(!deletedItem){
+    res.status(403)
+    return next(new Error("Tracker ID does not Exist"))
+}
+res.send(`${trackerId} has been deleted`)
+
+}
+)
+})
+
 module.exports = trackerRouter
