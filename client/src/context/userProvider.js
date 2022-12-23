359a51500b43f9d6b1d63e20f6c8ec0f9ba31003
Paul D'Amico <pauldamico88@gmail.com>
2022-12-23 00:18:30 -0500

    changed user intial state

--- client/src/context/userProvider.js
+++ client/src/context/userProvider.js
@@ -17,7 +17,7 @@ export function UserContextProvider(props) {
   };
   const [currentUser, setCurrentUser] = useState(initUser);
   const [loginError, setLoginError] = useState("")
-  const { _id: userId, username } = currentUser.user;
+  const { userId: _id, username } = currentUser.user;
   const { token } = currentUser;
   const navigate = useNavigate();
 
